65638d95a2ef6c98284782d58d40517fc795b64f
Sam Waters <sam@samwaters.com>
2024-05-20 16:19:09 +0100

    Add controls helper text

--- src/components/view/view.tsx
+++ src/components/view/view.tsx
@@ -1,5 +1,20 @@
-import { Box } from "@mui/material"
+import { Box, Typography } from "@mui/material"
 
 export const View = () => {
-  return <Box id="view-container" sx={{ display: "flex", justifyContent: "center" }}></Box>
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", gap: "10px", justifyContent: "center" }}>
+      <Box
+        id="view-container"
+        sx={{ display: "flex", justifyContent: "center", minHeight: "600px", minWidth: "800px" }}
+      ></Box>
+      <Typography variant="body1" component="div">
+        Controls
+      </Typography>
+      <Typography variant="body2" component="div">
+        Drag to pan around, scroll to zoom.
+        <br />
+        Select an object by clicking it, then drag to rotate and scroll to resize
+      </Typography>
+    </Box>
+  )
 }
